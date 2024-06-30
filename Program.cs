d325ac7e86ee5cfa7079afbceb4f88f357d17cb9
Radubtw <radu.badescu03@e-uvt.ro>
2024-06-30 19:29:11 +0300

    Added CSS to the Users page

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var app = builder.Build();
 builder.Services.AddDbContext<SmartWorkoutContext>();
+var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
