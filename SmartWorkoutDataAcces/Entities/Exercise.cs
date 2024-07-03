141d2914dcc6ff4bcb68702b170002e221a797a6
Radubtw <radu.badescu03@e-uvt.ro>
2024-07-03 10:14:11 +0300

    Added CRUD operations for exercises

--- SmartWorkoutDataAcces/Entities/Exercise.cs
+++ SmartWorkoutDataAcces/Entities/Exercise.cs
@@ -5,12 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace SmartWorkoutDataAccess.Entities
 {
     public class Exercise
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(@"[A-Za-z\s]{1,25}", ErrorMessage = "Invalid name")]
         public string Name { get; set; } = null!;
         public ICollection<Exercise_Log> Exercise_Logs { get; set; } = new HashSet<Exercise_Log>();
     }
--- SmartWorkoutDataAcces/Entities/User.cs
+++ SmartWorkoutDataAcces/Entities/User.cs
@@ -8,10 +8,12 @@ namespace SmartWorkoutDataAccess.Entities
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(@"[A-Za-z]{1,40}", ErrorMessage = "Invalid name")]
         public string Name { get; set; } = null!;
         [Required(ErrorMessage = "Surname is required")]
+        [RegularExpression(@"[A-Za-z]{1,40}", ErrorMessage = "Invalid surname")]
         public string Surname { get; set; } = null!;
-        [RegularExpression(@"^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]{9}$", ErrorMessage ="Invalid Phone number")]
+        [RegularExpression(@"^[\+]?[(]?[0-9]{1,3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage ="Invalid Phone number")]
         public string? Phone { get; set; }
         [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email format")]
         public string? Email { get; set; }
@@ -19,7 +21,6 @@ namespace SmartWorkoutDataAccess.Entities
         public int? Age { get; set; }
         [Range(1, 300, ErrorMessage = "Weight should be between 1 and 300")]
         public double? Weight { get; set; }
-
         public ICollection<Workout> Workouts { get; set; } = new HashSet<Workout>();
     }
 }
